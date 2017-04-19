de91baa2e92f1d9554316dabbbbb9abd7febef4f
FelSiq <felipe.siqueira@usp.br>
2017-04-19 12:56:55 -0300

    Changed L02 code.

--- laboratorio-de-fisica-ifsc/ifsc-pratica-02.R
+++ laboratorio-de-fisica-ifsc/ifsc-pratica-02.R
@@ -1,6 +1,7 @@
 #Resumo:
 #Parte 1: modulo de Young:
-#Calculo final: 16.2 10^10 Pa (por coef. angular) +- 0.3 10^10 Pa (por P e x)
+#Calculo final: 16.2 10^10 Pa (por coef. angular) +- 0.3 10^10 Pa 
+#(por P e x)
 #Material mais próximo, segundo a tabela da apostila: Aco
 
 #Constantes adotadas
@@ -22,7 +23,8 @@ MedidaInicialDaProveta <- 5.0 #em cm
 DeslocamentoReguaLFixoPVar <- c(5.8, 6.5, 7.3, 8.0, 8.7,
 	9.1, 9.8, 10.4)	#em cm
 
-LInicial <- 27.0 #L inicial da régua p/ medições de deformação com L variável, em cm
+LInicial <- 27.0 #L inicial da régua p/ medições de deformação com 
+#L variável, em cm
 LVariacao <- 3.0 #Variamos o L de aproximadamente 3.0 em 3.0 cm
 DeslocamentoReguaLVarPFixo <- c(10.4, 8.6, 7.2, 6.2, 5.4,
 	4.9, 4.6, 4.4, 4.4)	#em cm
@@ -36,11 +38,16 @@ UnsureYoung <- function(F, L, d, b, x, dF, dL, dd, db, dx){
 	(8.0 *(L^3)*F*dx)/((d^3)*b*(x^2))
 }
 
-AngularCoef <- function(x0, x1, y0, y1) (y1 - y0)/(x1 - x0)
-YoungModulus <- function(F, L, d, b, x) (F * 4.0 * (L^3))/((d^3) * b * x)
-YoungModulusByCoef <- function(m, L, d, b) (m * 4.0 * (L^3))/((d^3) * b)
-AngularCoefByYoung <- function(E, d, b, L) (E*(d^3)*b)/(4.0*(L^3))
-Equivalency <- function(x1, x2, y1, y2) abs(x1 - x2) < (2*(y1 + y2))
+AngularCoef <- function(x0, x1, y0, y1) 
+	(y1 - y0)/(x1 - x0)
+YoungModulus <- function(F, L, d, b, x) 
+	(F * 4.0 * (L^3))/((d^3) * b * x)
+YoungModulusByCoef <- function(m, L, d, b) 
+	(m * 4.0 * (L^3))/((d^3) * b)
+AngularCoefByYoung <- function(E, d, b, L) 
+	(E*(d^3)*b)/(4.0*(L^3))
+Equivalency <- function(x1, x2, y1, y2) 
+	abs(x1 - x2) < (2*(y1 + y2))
 
 #Parte 1: Determinação do módulo de Yong
 PesoObjeto <- round((MassaObjetosMetalicos / 1000.0) * Gravidade, 2)
@@ -53,7 +60,9 @@ TabelaPesoXDeformacao #Imprime a tabela
 plot(TabelaPesoXDeformacao) #Plota a tabela (evidencia relação linear entre as grandezas)
 #Adiciona a reta que representa o conjunto de dados
 abline(0, AngularCoef(0.55, 3.76, 0.8, 5.4), col="blue") 
+title("Gráfico 1 - Relação linear entre Peso (N) e a Deformação (cm) da barra de metal")
 
+#Parâmetros para o cálculo do Modulo de Young
 F <- PesoObjeto
 x <- (DeslocamentoReguaLFixoPVar - MedidaInicialDaProveta) * 10^(-2)
 L <- LInicial * 10^(-2)
@@ -103,11 +112,12 @@ UnsureYoung(PesoObjeto, L, d, b, DeslocamentoReguaLFixoPVar,
 #Usando o desvio padrao:
 sd(YModulus/10^10) #[1] 0.3116274 (+- 10^10 Pa)
 
+
 #Parte 2: Análise da relação comprimento-deformação
 TabelaLVariavel <- data.frame(seq(from = LInicial, 
 	to = 3.0, by = -LVariacao))
-TabelaLVariavel[2] <- DeslocamentoReguaLVarPFixo
-TabelaLVariavel[3] <- (TabelaLVariavel[[1]]^3)/100.0
+TabelaLVariavel[2] <- DeslocamentoReguaLVarPFixo - MedidaInicialDaProveta
+TabelaLVariavel[3] <- round((TabelaLVariavel[[1]]^3)/100.0, 1)
 colnames(TabelaLVariavel) = c("L (cm +- 0.1cm)", 
 	"x (cm +- 0.1cm)", "L^3 (m +- 10^-5m)")
 
@@ -116,11 +126,21 @@ plot(TabelaLVariavel)
 
 plot(TabelaLVariavel[[3]], TabelaLVariavel[[2]], 
 	type = 'b', xlab = "L^3 (m +- 10^-5m)", ylab = "x (cm +-0.1cm)")
-title("Relação entre L^3 (m) e x (cm)")
+title("Gráfico 2: Relação entre L^3 (m) e x (cm)")
 arrows(TabelaLVariavel[[3]], TabelaLVariavel[[2]]-0.1, 
 	TabelaLVariavel[[3]], TabelaLVariavel[[2]]+0.1, 
 	length=0.05, angle=90, code=3)
-#Comprova a relacao cubica 
+#Comprova a relacao linear entre L^3 e x 
 
-#Calcular o coef angular, fazer os gráficos em papel e 
-#Calcular o módulo de young E.
+m3 <- AngularCoef((TabelaLVariavel[7, 3])/10^2, 
+	(TabelaLVariavel[3, 3])/10^2, 
+	TabelaLVariavel[7, 2], 
+	(TabelaLVariavel[3, 2]))
+
+YModulus3 <- YoungModulusByCoef(m3, 
+	(TabelaLVariavel[3, 3] - TabelaLVariavel[7, 3])/10^2, 
+	d, b)/10^10
+#22.30575 10^10 Pa
+
+TabelaComparacaoYoung[3] <- round(abs(YModulus3 - TabelaComparacaoYoung[[1]]), 1)
+
